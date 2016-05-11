142366e79575bbd649e9bc96fd9db509eab4d83e
furyu-araki <araki.taku@furyu.jp>
2016-05-12 01:42:28 +0900

    サンプルのビルドが通るように修正

--- openCVSamplefacedetection/src/main/jni/Android.mk
+++ openCVSamplefacedetection/src/main/jni/Android.mk
@@ -12,7 +12,7 @@ ifdef OPENCV_ANDROID_SDK
     include ${OPENCV_ANDROID_SDK}/sdk/native/jni/OpenCV.mk
   endif
 else
-  include ../../sdk/native/jni/OpenCV.mk
+  include /Users/arakitaku/Library/Android/OpenCV-3.1.0-android-sdk/sdk/native/jni/OpenCV.mk
 endif
 
 LOCAL_SRC_FILES  := DetectionBasedTracker_jni.cpp
