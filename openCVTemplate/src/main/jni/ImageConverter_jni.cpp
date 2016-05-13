3f71eee77b79f4f63930be243b43ef3772b56fe6
furyu-araki <araki.taku@furyu.jp>
2016-05-13 19:39:24 +0900

    ポスタライズ処理

--- openCVTemplate/src/main/jni/ImageConverter_jni.cpp
+++ openCVTemplate/src/main/jni/ImageConverter_jni.cpp
@@ -13,19 +13,39 @@
 using namespace std;
 using namespace cv;
 
+unsigned char table[256];
+bool tableInitFlag = false;
+
+int posterize_intensity(int intensity, int poster_level){
+	int bin_size = 256 / poster_level;
+	return (intensity / bin_size) * (255 / (poster_level - 1));
+}
+
+void initTable()
+{
+    if(tableInitFlag){
+        return;
+    }
+    for(int color = 0; color < 256; ++color){
+        table[color] = color / 128 * 128;
+    }
+    tableInitFlag = true;
+}
 
 JNIEXPORT void JNICALL Java_org_opencv_template_convert_ImageConverter_nativeConvert
 (JNIEnv * jenv, jclass, jlong imagePtr)
 {
     LOGD("Java_org_opencv_template_convert_ImageConverter_nativeConvert");
 
-    try
-    {
-        cv::Mat image = *((Mat*)imagePtr);
-    }
-    catch (...)
-    {
-        LOGD("nativeConvert caught unknown exception");
-    }
+    initTable();
 
+    cv::Mat image = *((Mat*)imagePtr);
+
+    for(int y = 0; y < image.rows; ++y){
+    	for(int x = 0; x < image.cols; ++x){
+            image.data[ y * image.step + x * image.elemSize() + 0 ] = table[ image.data[ y * image.step + x * image.elemSize() + 0 ] ];
+            image.data[ y * image.step + x * image.elemSize() + 1 ] = table[ image.data[ y * image.step + x * image.elemSize() + 1 ] ];
+            image.data[ y * image.step + x * image.elemSize() + 2 ] = table[ image.data[ y * image.step + x * image.elemSize() + 2 ] ];
+    	}
+    }
 }
