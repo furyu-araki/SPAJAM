c7db7e3f800ba150d4408fc70b9aeff49991ac2d
umama0716 <rustydeer@gmail.com>
2016-05-14 19:50:33 +0900

    結果画面を実装

--- openCVTemplate/src/main/java/org/opencv/template/ResultActivity.java
+++ openCVTemplate/src/main/java/org/opencv/template/ResultActivity.java
@@ -2,6 +2,7 @@ package org.opencv.template;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 /**
  * Created by arakitaku on 2016/05/14.
@@ -12,6 +13,11 @@ public class ResultActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        //SurfaceViewをセットする
+        LinearLayout l = new LinearLayout(this);
+        setContentView(l);
+        l.addView(new ParaparaSurfaceView(this));
     }
 
 }
